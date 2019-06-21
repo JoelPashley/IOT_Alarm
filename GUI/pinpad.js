a6259448e13e04525461cf1ac991d6571639e267
Joel <joel.pashley@hotmail.co.uk>
2019-06-21 14:20:39 +0100

    Front end complete.
    Front end functionality is complete, UI can now send and receive information from the Node server, need to implement sensors and alarm physically and link their listeners to the events in the Node server.

---
+++ GUI/pinpad.js
@@ -0,0 +1,114 @@
+var socket = io();
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+$(document).ready(function(){
+    Push.Permission.request(null, null);
+    socket.on("connected", function(data){
+        if(data === true){
+            $(".STATUSBAR").text("STATUS: ARMED");
+        }else if(data === false){
+            $(".STATUSBAR").text("STATUS: DISARMED");
+        }else{
+            $(".STATUSBAR").text("STATUS: TRIGGERED - "+data);
+        }
+    });
+
+    $(".pin-button").click(function(){
+        var text = $(".PINCODE").text();
+        if(text.substr(0,1) === "#"){
+            $(".PINCODE").text(setCharAt(text, 0, $(this).text()));
+            return;
+        }else if(text.substr(1,1) === "#"){
+            $(".PINCODE").text(setCharAt(text, 1, $(this).text()));
+            return;
+        }else if(text.substr(2,1) === "#"){
+            $(".PINCODE").text(setCharAt(text, 2, $(this).text()));
+            return;
+        }else if(text.substr(3,1) === "#"){
+            $(".PINCODE").text(setCharAt(text, 3, $(this).text()));
+        }
+        if($(".PINCODE").text().substr(3,1) != "#"){
+            socket.emit("passcode", $(".PINCODE").text());
+        }
+    });
+
+    $("#ARM").click(function(){
+        if($(this).hasClass("clickable")){
+            var passcode = $(".PINCODE").text();
+            $(".PINCODE").text("ARMED");
+            $("#DISARM").removeClass("clickable");
+            $("#ARM").removeClass("clickable");
+            window.setTimeout(function(){
+                $(".PINCODE").text(passcode);
+                $("#DISARM").addClass("clickable");
+                $("#ARM").addClass("clickable");
+                socket.emit('armed', passcode);
+            }, 1500);
+        }
+    });
+
+    $("#DISARM").click(function(){
+        if($(this).hasClass("clickable")){
+            var passcode = $(".PINCODE").text();
+            $(".PINCODE").text("DISARMED");
+            $("#ARM").removeClass("clickable");
+            $("#DISARM").removeClass("clickable");
+            window.setTimeout(function(){
+                $(".PINCODE").text(passcode);
+                $("#ARM").addClass("clickable");
+                $("#DISARM").addClass("clickable");
+                socket.emit('disarmed', passcode);
+            }, 1500);
+        }
+    });
+
+    socket.on("passwordRes", function(data){
+        if(data == true){
+            $(".PINCODE").addClass("SUCCESS");
+            $(".disabled").addClass("clickable");
+            $(".disabled").removeClass("disabled");
+        }else{
+            $(".PINCODE").addClass("FAIL");
+            window.setTimeout(resetPINPAD, 2500);
+        }
+    });
+    
+    socket.on("armedRes", function(data){
+        if(data){
+            $(".STATUSBAR").text("STATUS: ARMED");
+            Push.create("Alarm Notification", {
+                body: "The alarm has been armed.",
+                icon: 'GUI/alarmicon.png',
+                timeout: 10000,
+                onClick: function(){
+                    window.focus();
+                    this.close();
+                }
+            });
+        }
+    });
+    
+    socket.on("disarmedRes", function(data){
+        if(data){
+            $(".STATUSBAR").text("STATUS: DISARMED");
+            Push.create("Alarm Notification", {
+                body: "The alarm has been disarmed.",
+                icon: 'GUI/alarmicon.png',
+                timeout: 10000,
+                onClick: function(){
+                    window.focus();
+                    this.close();
+                }
+            });
+        }
+    });
+});
+
+function resetPINPAD(){
+    $(".PINCODE").text("####");
+    $(".PINCODE").removeClass("FAIL");
+}
--- index.js
+++ index.js
@@ -2,43 +2,87 @@ var FS = require('fs');
 var http = require('http').createServer(handler);
 var url = require('url');
 var io = require('socket.io')(http);
+var favicon = require('serve-favicon');
 
 http.listen(80);
 
+var ARMED = false;
+var ACTIVE = false;
+var ACTIVE_START = ""+new Date().getHours()+":"+new Date().getMinutes();
+
+var _favicon = favicon("GUI/favicon.ico");
+
 //This function creates the http server and will pass only ever serve the http file GUI.html.
 function handler(req, res){
-    var query = url.parse(req.url, true);
-    var filename = query.pathname;
-    if(filename == "/GUI/main.css"){
-        FS.readFile('GUI/main.css', function(err, data){
-            res.writeHead(200, {'Content-Type':'text/css'});
-            res.write(data);
-            res.end();
-        });
-    }else if(filename == "GUI/pinpad.js"){
-        FS.readFile('GUI/pinpad.js', function(err, data){
-            res.writeHead(200, {'Content-Type':'text/javascript'});
-            res.write(data);
-            res.end();
-        });
-    }else{
-        FS.readFile('GUI/GUI.html', function(err, data){
-            res.writeHead(200, {'Content-Type':'text/html'});
-            res.write(data);
-            res.end();
-        });
-    }
+    _favicon(req, res, function(){
+        var query = url.parse(req.url, true);
+        var filename = query.pathname;
+        if(filename == "/GUI/main.css"){
+            FS.readFile('GUI/main.css', function(err, data){
+                res.writeHead(200, {'Content-Type':'text/css'});
+                res.write(data);
+                res.end();
+            });
+        }else if(filename == "/GUI/pinpad.js"){
+            FS.readFile('GUI/pinpad.js', function(err, data){
+                res.writeHead(200, {'Content-Type':'text/javascript'});
+                res.write(data);
+                res.end();
+            });
+        }else if(filename == "/GUI/alarmicon.png"){
+            FS.readFile('GUI/alarmicon.png', function(err, data){
+                res.writeHead(200, {'Content-Type':'image'});
+                res.write(data);
+                res.end();
+            });
+        }else{
+            FS.readFile('GUI/GUI.html', function(err, data){
+                res.writeHead(200, {'Content-Type':'text/html'});
+                res.write(data);
+                res.end();
+            });
+        }
+    });
 }
 
 io.sockets.on('connection', function(socket){
     var passcode = "1234";
+    if(ACTIVE){
+        socket.emit("connected", ACTIVE_START);
+    }else{
+        socket.emit("connected", ARMED);
+    }
     socket.on('passcode', function(data){
         if(data == passcode){
             socket.emit('passwordRes', true);
         }else{
             socket.emit('passwordRes', false);
         }
-    })
+    });
+    socket.on('armed', function(data){
+        if(data == passcode){
+            //IMPLEMENT: ARM ALARM ACTIVATE PIR AND ALARM
+            ARMED = true;
+            io.sockets.emit('armedRes', true);
+        }else{
+            socket.emit('passwordRes', false);
+        }
+    });
+    socket.on('disarmed', function(data){
+        if(data == passcode){
+            //IMPLEMENT: ARM ALARM DEACTIVATE PIR AND ALARM
+            ARMED = false;
+            io.sockets.emit('disarmedRes', true);
+        }else{
+            socket.emit('passwordRes', false);
+        }
+    });
+});
+
+process.on('SIGINT', function(){
+    //IMPLEMENT: DEACTIVATE AND UNEXPORT ALL GPIO
+    console.log("Unloaded... Exiting...")
+    process.exit();
 })
 
 console.log("Server started...")
